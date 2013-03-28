abdd431f627eda3d1c106fd99ba30aba694e3afe
Anthony Topper <ttopper@gmail.com>
2013-03-28 16:19:02 -0400

    Initial setup of child theme

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+require 'susy'
+
+# Set this to the root of your project when deployed:
+http_path = "/wp-content/themes/html5_boilerplate_wp_compass"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+output_style = :compact 
